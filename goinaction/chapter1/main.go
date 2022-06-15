563b6594f5f3d23f87a0a2fc2cc32b01faccd65b
zhangpengpeng <zhan_gpen_g@163.com>
2022-06-15 17:03:30 +0800

    save

--- goinaction/chapter1/main.go
+++ goinaction/chapter1/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"go-code/goinaction/chapter1/search"
-	"log"
-	"os"
-
+	"fmt"
+	"go-code/goinaction/chapter1/pubsub"
 )
 
-func main() {
-	search.Run("president")
-}
+//func main() {
+//	search.Run("president")
+//}
+//
+//func init() {
+//	log.SetOutput(os.Stdout)
+//}
 
-func init() {
-	log.SetOutput(os.Stdout)
+
+func main() {
+	ch := pubsub.GenerateNatural() // 自然数序列: 2, 3, 4, ...
+	for i := 0; i < 100; i++ {
+		prime := <-ch // 新出现的素数
+		fmt.Printf("%v: %v\n", i+1, prime)
+		ch = pubsub.PrimeFilter(ch, prime) // 基于新素数构造的过滤器
+	}
 }
---
+++ goinaction/chapter1/pubsub/su.go
@@ -0,0 +1,26 @@
+package pubsub
+
+
+// 返回生成自然数序列的管道: 2, 3, 4, ...
+func GenerateNatural() chan int {
+	ch := make(chan int)
+	go func() {
+		for i := 2; ; i++ {
+			ch <- i
+		}
+	}()
+	return ch
+}
+
+// 管道过滤器: 删除能被素数整除的数
+func PrimeFilter(in <-chan int, prime int) chan int {
+	out := make(chan int)
+	go func() {
+		for {
+			if i := <-in; i%prime != 0 {
+				out <- i
+			}
+		}
+	}()
+	return out
+}
